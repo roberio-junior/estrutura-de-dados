6ca8c293c399125608a9fa2ba2b9eddd32ff95db
Robério Júnior <roberiojr110@gmail.com>
2025-05-28 11:32:04 -0300

    correção dos exemplos das funções

--- Recursividade/fatorial.py
+++ Recursividade/fatorial.py
@@ -1,13 +1,4 @@
-import time
 def fatorial(n):
     if n == 1:
         return 1
     return n * fatorial(n-1)
-
-numero = int(input('Digite o número que deseja encontrar o seu fatorial: '))
-
-inicio = time.time()
-resultado = fatorial(numero)
-final = time.time()
-print(f"O fatorial de {numero} é {resultado}")
-print(final-inicio)
--- Recursividade/potencia.py
+++ Recursividade/potencia.py
@@ -1,16 +1,4 @@
-import time
-
 def pot (n: int, p: int) -> int:
     if p == 1:
         return n
     return n * pot(n, p-1)
-
-numero = int(input('Digite o número que deseja encontrar a sua pontencia: '))
-potencia = int(input('Digite a potencia desejada: '))
-
-inicio = time.time()
-resultado = pot(numero, potencia)
-final = time.time()
-
-print(f"A potência de {potencia} na base {numero} é: {resultado}")
-print(f"Tempo de execução: {final-inicio}")
