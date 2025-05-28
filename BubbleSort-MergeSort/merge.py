b1e7cc51e59b5155510ba096c975789106ee93ed
Robério Júnior <roberiojr110@gmail.com>
2025-05-28 09:25:24 -0300

    comentários em merge

--- BubbleSort-MergeSort/merge.py
+++ BubbleSort-MergeSort/merge.py
@@ -1,9 +1,15 @@
+# Função que combina os conceitos de Merge Sort e Shell Sort
 def merge_sort (lista: list[int]) -> list[int]:
+    # Se a lista tiver apenas um elemento, ela já está ordenada
     if len(lista) == 1:
         return lista
+    # Divide a lista ao meio
     metade  = len(lista) // 2
+    # Aplica merge_sort recursivamente nas duas metades
+    # Junta as duas metades ordenadas e ordena o resultado com shell_sort
     return shell_sort(merge_sort(lista[:metade]) + merge_sort(lista[metade:]))
 
+# Função de ordenação Shell Sort
 def shell_sort(lista: list[int]) -> list[int]:
     n = len(lista)
     gap = n // 2
@@ -20,5 +26,8 @@ def shell_sort(lista: list[int]) -> list[int]:
 
     return lista
 
+# Lista de exemplo
 lista = [54,26,93,17,77,31,44,55,20]
-print(merge_sort(lista))
+
+# Imprime a lista original ordenada utilizando a função merge_sort
+print("Lista ordenada:", merge_sort(lista))
